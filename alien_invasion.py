7651d3327a13241c3ce0960c2bb5d94c9fbf172e
Wstreet <wstreet1@student.cscc.edu>
2025-07-20 00:29:02 -0400

    Created game window with ability to open and close

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,24 @@
 import sys
 import pygame
 
+class AlienInvasion:
+
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        self.running = True
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.flip()
 if __name__ == "__main__":
-    """Test comment"""
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
