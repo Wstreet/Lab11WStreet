241e1d66d86cdfb532dbcde65b610ba09461ab67
Wstreet <wstreet1@student.cscc.edu>
2025-07-23 20:18:39 -0400

    altered background imported things created settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,25 @@
 import sys
 import pygame
+from setting import Settings
 
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((self.settings.screen_w, self.settings.screen_h))
+
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, (self.settings.screen_w, self.settings.screen_h))
+
+        self.running = True
+        self.clock = pygame.time.Clock()
 
     def run_game(self):
-        self.running = True
+
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -18,7 +27,10 @@ class AlienInvasion:
                     pygame.quit()
                     sys.exit()
 
-            pygame.display.flip()
+            self.screen.blit(self.bg, (0, 0))
+            pygame.display.flip() 
+            self.clock.tick(self.settings.FPS)
+
 if __name__ == "__main__":
     ai = AlienInvasion()
     ai.run_game()
