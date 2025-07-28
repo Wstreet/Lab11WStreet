fbf5bf18fe483d68c2796ed26e994c52e4197f92
Wstreet <wstreet1@student.cscc.edu>
2025-07-27 21:00:37 -0400

    Alien bounces now

--- alien.py
+++ alien.py
@@ -10,20 +10,34 @@ class Alien(Sprite):
         super().__init__()
 
         self.screen = game.screen
-        self.settings = game.settings
         self.boundaries = game.screen.get_rect()
+        self.settings = game.settings
+
 
         self.image = pygame.image.load(self.settings.alien_file)
         self.image = pygame.transform.scale(
         self.image, (self.settings.alien_w, self.settings.alien_h)
         )
+
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
         self.y = self.rect.y
+        self.x = self.rect.x
 
     def update(self):
-        pass
+        temp_speed = self.settings.fleet_speed
+
+        if self.check_edges():
+            self.settings.fleet_direction *=-1
+
+        self.x += temp_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+
+    def check_edges(self):
+        return(self.rect.right >= self.boundaries.right or self.rect.left <= self.boundaries.left)
+
 
     def draw_alien(self):
         self.screen.blit(self.image, self.rect)
